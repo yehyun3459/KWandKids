0d28984fa7c77cdd644c393314259d20a149c5ab
kihun123 <46311298+kihun123@users.noreply.github.com>
2020-02-04 18:07:59 +0900

    Update kkh.cpp

--- Baekjoon/P17140 (이차원 배열과 연산)/kkh.cpp
+++ Baekjoon/P17140 (이차원 배열과 연산)/kkh.cpp
@@ -2,38 +2,38 @@
 #include <algorithm>
 using namespace std;
 int r, c, k, Arr[201][201], row_size, col_size;
-void col() {
-	int tmp_row_cnt = row_size;
+void col() {//열 정렬
+	int tmp_row_cnt = row_size; //행 개수 임시 저장
 	for (int j = 0; j < col_size; j++) {
-		pair<int, int> p[101] = { { 0, 0 } };
+		pair<int, int> p[101] = { { 0, 0 } }; //(반복횟수, 숫자)
 		int maxx = 0;
 		for (int i = 0; i < tmp_row_cnt; i++) {
 			if (Arr[i][j]) {
 				p[Arr[i][j]].first++;
 				p[Arr[i][j]].second = Arr[i][j];
-				maxx = Arr[i][j] > maxx ? Arr[i][j] : maxx;
+				maxx = Arr[i][j] > maxx ? Arr[i][j] : maxx;//젤 큰 숫자 저장
 			}
 		}
-		sort(p + 1, p + maxx + 1);
+		sort(p + 1, p + maxx + 1);//오름차순 정렬 횟수 기준으로
 		int idx = 0;
 		for (int i = 1; i <= maxx; i++) {
-			if (p[i].second) {
+			if (p[i].second) {//0값 제외 후 횟수적은 순으로 입력
 				Arr[idx][j] = p[i].second;
 				Arr[idx+1][j] = p[i].first;
 				idx += 2;
 			}
 		}
-		for (int i = idx; i < tmp_row_cnt * 2; i++)
+		for (int i = idx; i < tmp_row_cnt * 2; i++) //열 사이즈 줄어들 경우를 위해 0값으로 추가 
 			Arr[i][j] = 0;
-		if (row_size < idx)
+		if (row_size < idx) //정렬한 열 크기 중 젤 높은 값 설정
 			row_size = idx;
 	}
 }
 
-void row() {
-	int tmp_col_cnt = col_size;
+void row() {//행 정렬
+	int tmp_col_cnt = col_size;//열 개수 임시저장
 	for (int j = 0; j < row_size; j++) {
-		pair<int, int> p[101] = { { 0, 0 } };
+		pair<int, int> p[101] = { { 0, 0 } };//(횟수, 숫자)
 		int maxx = 0;
 		for (int i = 0; i < tmp_col_cnt; i++) {
 			if (Arr[j][i]) {
@@ -42,7 +42,7 @@ void row() {
 				maxx = Arr[j][i] > maxx ? Arr[j][i] : maxx;
 			}
 		}
-		sort(p + 1, p + maxx + 1);
+		sort(p + 1, p + maxx + 1);//횟수별 정렬
 		int idx = 0;
 		for (int i = 1; i <= maxx; i++) {
 			if (p[i].second) {
